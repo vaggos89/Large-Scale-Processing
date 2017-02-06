67a95f0e72640e8e4fb2c05eb621ab519e6dbe18
vaggos89 <vmitsian@hotmail.com>
2017-02-06 21:14:41 +0200

    k_means_final_ver

---
+++ k_means_v2.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+# scikit-learn
+from scipy.sparse import csr_matrix
+from sklearn.metrics.pairwise import euclidean_distances, cosine_distances
+import csv
+
+import matplotlib.pyplot as plt
+
+
+def rand_representatives(data, num_repr):
+
+    # Initialize representatives randomly
+
+    # Create an array [1, num_of_articles]
+    temp_array = np.array(range(data.shape[0]))
+
+    # Randomly shuffle the array
+    np.random.shuffle(temp_array)
+
+    # Select the firs 5, random representatives
+
+    return data[temp_array[0:num_repr]]
+
+
+def seq_representatives(data, num_repr):
+
+    # Initialize representatives using a sequential
+
+    return data
+
+
+def longest_dist_representatives(data, num_per):
+
+    # Initialize representatives using a sequential
+
+    return data
+
+
+def calc_dist(flag, data, curr_repr):
+
+    # Calculate distances between the data and the representatives
+
+    if flag == 0:
+        return euclidean_distances(data, curr_repr)
+
+    return cosine_distances(data, curr_repr)
+
+
+def update_clusters(dist):
+
+    # Update the clusters and the corresponding labels
+
+    return np.argmin(dist, axis=1)
+
+
+def update_representatives(data, clust_lab, curr_repr, clu_num):
+
+    # Update the representatives of each cluster
+    prev_repr = csr_matrix.copy(curr_repr)
+
+    for i in range(clu_num):
+        curr_repr[i] = csr_matrix(data[clust_lab == i].mean(axis=0, dtype=np.float64))
+
+    similarity = (prev_repr - curr_repr).nnz
+    if similarity == 0:
+        return curr_repr, True
+    else:
+        return curr_repr, False
+
+
+def print_Results(cluster_labels, T_labels, clu_num):
+
+    results = np.zeros((clu_num, clu_num), dtype=np.float64)
+
+    for i in range(clu_num):
+        tmp = T_labels[cluster_labels == i]
+        for j in range(clu_num):
+            results[i, j] = (len(tmp[tmp == j+1])/float(len(tmp)))*100
+
+    # print results to csv file
+    with open('clustering_KMeans.csv', 'w') as fp:
+        a = csv.writer(fp, delimiter=',')
+        a.writerows([[' ', 'Politics', 'Film', 'Business', 'Technology', 'Football']])
+        i = 1
+        for row in results:
+            string = 'Cluster %d' % i
+            a.writerows([[string, '{: 0.2f}'.format(row[0]), '{: 0.2f}'.format(row[1]), '{: 0.2f}'.format(row[2]), '{: 0.2f}'.format(row[3]), '{: 0.2f}'.format(row[4])]])
+            i += 1
+    # print results as png image
+    fig = plt.figure(1)
+    plt.pcolormesh(results, cmap='RdBu')
+    plt.colorbar()
+    plt.gca().invert_yaxis()
+    plt.title(' Politics       Film     BusinessTechnologyFootball')
+    plt.ylabel('Clusters')
+    plt.show()
+    fig.savefig('clustering_KMeans.png')
+
+
+# Main program
+
+# Load sparse matrix
+
+path = '/home/ubuntu/Desktop/Large_Scale_Tech/sparse_data_norm.npz'
+# path = '/home/apostolis/Desktop/Large_Scale_Tech/sparse_data_norm.npz'
+
+loader = np.load(path)
+sparse_data_norm = csr_matrix((loader['data'], loader['indices'], loader['indptr']), shape=loader['shape'])
+
+# Load Labels array
+
+path = '/home/ubuntu/Desktop/Large_Scale_Tech/labels_arr.npy'
+# path = '/home/apostolis/Desktop/Large_Scale_Tech/labels_arr.npy'
+
+labels_arr = np.load(path)
+
+# Initialize representatives according to initialization method
+num_of_repr = 5
+init_flag = 0
+
+if init_flag == 0:
+    representatives = rand_representatives(sparse_data_norm, num_of_repr)
+elif init_flag == 1:
+    representatives = seq_representatives(sparse_data_norm, num_of_repr)
+else:
+    representatives = longest_dist_representatives(sparse_data_norm, num_of_repr)
+
+# k-means loop
+
+# Set 0 for euclidean, 1 for Cosine
+dist_flag = 0
+MAX_ITER = 1000
+
+for itr in range(MAX_ITER):
+    # Calculate distances between the data and the representatives
+    distances = calc_dist(dist_flag, sparse_data_norm, representatives)
+    # print distances.shape
+
+    # Calculate the clusters
+    cluster_labels = update_clusters(distances)
+    # print cluster_labels
+    # print cluster_labels.shape
+
+    # Calculate the new representatives
+    representatives, terminate = update_representatives(sparse_data_norm, cluster_labels, representatives, num_of_repr)
+    # print representatives.shape
+
+    # terminate if the representatives don't change
+    if terminate:
+        break
+
+print_Results(cluster_labels, labels_arr, num_of_repr)
---
+++ read_data_v2.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+
+import csv
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+# sklearn
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import normalize
+
+# wordCloud
+from wordcloud import WordCloud, STOPWORDS
+
+
+def find_label(string):
+
+    # convert name category to integer for faster process
+
+    if string == 'Politics':
+        label = 1
+    elif string == 'Film':
+        label = 2
+    elif string == 'Business':
+        label = 3
+    elif string == 'Technology':
+        label = 4
+    else:
+        label = 5
+
+    return label
+
+
+def separate_cat_text(file_name):
+
+    # Extract from csv the fields titles, labels, texts
+
+    lst_title = []
+    lst_label = []
+    lst_text = []
+    lst_id = []
+    with open(file_name, 'r') as train_csv:
+        reader = csv.DictReader(train_csv, delimiter='\t')
+        for row in reader:
+            lst_title.append(row['Title'])
+            lst_label.append(find_label(row['Category']))
+            lst_text.append(row['Content']+' '+row['Title'])
+            lst_id.append(row['Id'])
+
+    labels_arr = np.array([x for x in lst_label])
+
+    # Store the appropriate data for later use
+
+    path = '/home/ubuntu/Desktop/Large_Scale_Tech/'
+    # path = '/home/apostolis/Desktop/Large_Scale_Tech/'
+
+    with open(path + 'titles_lst', 'wb') as f:
+        pickle.dump(lst_title, f)
+
+    with open(path + 'text_lst', 'wb') as f:
+        pickle.dump(lst_text, f)
+
+    with open(path + 'id_lst', 'wb') as f:
+        pickle.dump(lst_id, f)
+
+    np.save(path + 'labels_arr', labels_arr)
+
+    # Return values
+    return lst_title, labels_arr, lst_text, lst_id
+
+
+def deconstruct_initial_data():
+
+    # Generate the appropriate data files and store for later use
+
+    path = '/home/ubuntu/Desktop/Large_Scale_Tech/train_set.csv'
+    # path = '/home/apostolis/Desktop/Large_Scale_Tech/train_set.csv'
+
+    # Separate data from csv
+    titles, labels, texts, ids = separate_cat_text(path)
+
+    # For each category generate a file with corresponding contents
+    for i in range(5):
+
+        path = '/home/ubuntu/Desktop/Large_Scale_Tech/text_%d.txt'%(i+1)
+        # path = '/home/apostolis/Desktop/Large_Scale_Tech/text_%d.txt'%(i+1)
+        target = open(path, 'w')
+
+        j = 0
+        for text in texts:
+            if labels[j] == i+1:
+                target.write(text)
+            j += 1
+        target.close()
+
+    return titles, texts
+
+
+def create_wordcloud():
+
+    # Create a wordcloud for each category
+
+    path = '/home/ubuntu/Desktop/Large_Scale_Tech/ranksnl_stopwords.txt'
+    # path = '/home/apostolis/Desktop/Large_Scale_Tech/ranksnl_stopwords.txt'
+
+    f = open(path)
+    stopwords1 = f.read()
+    stopwords1 = stopwords1.split('\n')
+    stopwords1 = set(stopwords1)
+    f.close()
+
+    stopwords2 = set(STOPWORDS)
+    stopwords = stopwords1.union(stopwords2)
+
+    path = '/home/ubuntu/Desktop/Large_Scale_Tech/stopwords'
+    # path = '/home/apostolis/Desktop/Large_Scale_Tech/stopwords'
+
+    with open(path, 'wb') as f:
+        pickle.dump(stopwords, f)
+
+    for i in range(5):
+
+        path = '/home/ubuntu/Desktop/Large_Scale_Tech/text_%d.txt'%(i+1)
+        # path = '/home/apostolis/Desktop/Large_Scale_Tech/text_%d.txt'%(i+1)
+
+        text = open(path).read()
+
+        wordcloud = WordCloud(background_color='white', scale=1, width=800, height=600, stopwords=stopwords, max_words=200).generate(text)
+
+        plt.figure()
+        plt.imshow(wordcloud)
+        plt.axis("off")
+        path = '/home/ubuntu/Desktop/Large_Scale_Tech/WordCloud_%d.tiff'%(i+1)
+        # path = '/home/apostolis/Desktop/Large_Scale_Tech/WordCloud_%d.tiff'%(i+1)
+        plt.savefig(path)
+        # plt.show()
+        plt.close()
+
+    return stopwords
+
+
+def generate_sparse_data(stopwords, texts_plus_titles):
+
+    vectorizer = CountVectorizer(analyzer='word', stop_words=stopwords)
+    sparse_data = vectorizer.fit_transform(texts_plus_titles)
+
+    # normalize data
+    sparse_data_norm = normalize(sparse_data, norm='l2', axis=1)
+
+    # save sparse matrix
+
+    path = '/home/ubuntu/Desktop/Large_Scale_Tech/sparse_data_norm'
+    # path = '/home/apostolis/Desktop/Large_Scale_Tech/sparse_data_norm'
+
+    np.savez(path, data=sparse_data_norm.data, indices=sparse_data_norm.indices, indptr=sparse_data_norm.indptr, shape=sparse_data_norm.shape )
+    # np.savez('X_array_norm', data=sA.data, indices=sA.indices, indptr=sA.indptr, shape=sA.shape )
+
+
+# Main Program
+
+print 'Preprocessing the data..'
+
+start_t = time.time()
+
+titles, texts = deconstruct_initial_data()
+stopw = create_wordcloud()
+generate_sparse_data(stopw, texts)
+
+end_t = time.time()
+
+print 'Preprocessing was completed successfully!'
+print 'Execution time (in seconds): ', end_t - start_t
